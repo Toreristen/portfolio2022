0bc797513b209ef895640c4e2fa14a30cae8cb89
Toreristen <82028481+Toreristen@users.noreply.github.com>
2022-02-13 23:01:42 +0100

    navbar css is finished but js is buggy

--- js/burger.js
+++ js/burger.js
@@ -1,11 +1,27 @@
 const navSlide = () => {
-    const burger = document.querySelector('.nav__burger');
+    const burger = document.querySelector(".nav__burger");
     const nav = document.querySelector(".nav__list");
+    //nav.style.transform = "translateX(100%)";
     
+    //const mediaQuery = window.matchMedia("(min-width: 768px)");
+    
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        nav.style.transform = "translateX(100%)";
+    } else {
+        nav.style.transform = "translateX(0%)";
+    }
+        
     //toggle
-    burger.addEventListener('click', () => {
-        nav.classList.toggle(".nav-active");
-        console.log("works");
+    burger.addEventListener("click", () => {
+        
+        if (nav.style.transform === "translateX(100%)") {
+            nav.style.transform = "translateX(0%)";
+        } else {
+            nav.style.transform = "translateX(100%)";
+        }
+        
+        //nav.classList.toggle('nav__active');
+        burger.classList.toggle('toggle');
     })
 }
 
