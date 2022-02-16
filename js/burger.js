2eb34aa25f811a8c9f52eea075ad82f0b5693f39
Toreristen <82028481+Toreristen@users.noreply.github.com>
2022-02-16 19:44:23 +0100

    navbar is linked and about section started

--- js/burger.js
+++ js/burger.js
@@ -1,14 +1,38 @@
 const navSlide = () => {
     const burger = document.querySelector(".nav__burger");
     const nav = document.querySelector(".nav__list");
+    const menu1 = document.querySelector(".nav__list-a");
+    const menu2 = document.querySelector("#menu2");
+    const menu3 = document.querySelector("#menu3");
+    const menu4 = document.querySelector("#menu4");
     
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    //const mediaQuery = window.matchMedia("(max-width: 768px)");
     
     //toggle
     burger.addEventListener("click", () => {
         nav.classList.toggle("nav__active");
         burger.classList.toggle('toggle');
     })
+
+    menu1.addEventListener("click", () => {
+        nav.classList.toggle("nav__active");
+        burger.classList.toggle('toggle');
+    } )
+
+    menu2.addEventListener("click", () => {
+        nav.classList.toggle("nav__active");
+        burger.classList.toggle('toggle');
+    } )
+
+    menu3.addEventListener("click", () => {
+        nav.classList.toggle("nav__active");
+        burger.classList.toggle('toggle');
+    } )
+
+    menu4.addEventListener("click", () => {
+        nav.classList.toggle("nav__active");
+        burger.classList.toggle('toggle');
+    } )
 }
 
 navSlide();
